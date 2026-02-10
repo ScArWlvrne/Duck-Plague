f4947fb4eea5c3db6935fe35e4b1e83c814ff5f4
ScArWlvrne <kchemccown@gmail.com>
2026-02-09 16:17:42 -0800

    Stop tracking Qt/CMake build artifacts

--- build/Qt_6_10_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_DuckPlague_FILE /Users/kyran/Documents/Documents - Kyran’s MacBook Air/College/CS433/Duck-Plague/build/Qt_6_10_2_for_macOS-Debug/DuckPlague)
-set(__QT_DEPLOY_TARGET_DuckPlague_TYPE EXECUTABLE)
